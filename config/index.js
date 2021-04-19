e6bd53776d480e5a4e6129f8c31da10a7db5e059
Mingjie Liao <316906528@qq.com>
2021-04-19 21:09:00 +0800

    测试代码包上传

--- config/index.js
+++ config/index.js
@@ -48,7 +48,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, "../dist"),
-    assetsSubDirectory: "static",
+    assetsSubDirectory: "front-end-knowledge/static",
     assetsPublicPath: "/",
 
     /**
