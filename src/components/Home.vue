c2e488eadf62228d8e50f3e01763fb1ae7297507
Mingjie Liao <316906528@qq.com>
2021-04-21 22:25:01 +0800

    引入sass loader及出现的问题和解决办法

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -59,7 +59,7 @@ export default {
   }
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .router-style {
   text-decoration: none;
 }
--- src/components/WebpackPage.vue
+++ src/components/WebpackPage.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>hello is layout {{ pageKey }}</div>
+  <div class="container">
+    <el-page-header @back="goBack" title="返回首页" content="Webpack知识">
+    </el-page-header>
+    <el-container> 
+      <section class="section-container">
+        <h4>1. vue2.x项目配置scss</h4>
+        <p class="words-show-container">直接：npm install -D node-sass sass-loader，结果npm run dev报错：vue.js - TypeError: this.getOptions is not a function 
+          查看这两者版本：node-sass^5.0.0 sass-loader^11.0.7 版本问题，都说11.0.7不兼容node-sass的5版本
+          最终找到解决方案："node-sass": "^4.14.1", "sass-loader": "^7.0.3"
+          ps：webpack的很多包都存在兼容性问题，例如：webpack和webpack-cli版本兼容的问题，着实难用，而且官方并没有公布哪些版本兼容
+        </p>
+      </section>
+    </el-container>
+  </div>
 </template>
 <script>
 export default {
@@ -11,6 +24,29 @@ export default {
   created() {
     this.pageKey = this.$route.params.pagekey;
     console.log(this.$route.params.pagekey);
+  },
+  methods: {
+    goBack() {
+      this.$router.back();
+    },
+    runData() {}
   }
 };
 </script>
+<style lang="scss" scoped>
+.container {
+  width: calc(100% - 60px);
+  margin: 10px 30px 0;
+  .section-container {
+    width: 100%;
+  .code-show-container {
+    white-space: pre;
+    background-color: #f2f2f2;
+  }
+  .words-show-container {
+    white-space: pre;
+    background-color: #ffffff;
+  }
+  }
+}
+</style>
