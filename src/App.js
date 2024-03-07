824c4148a9cc16b0755bbb1ff83131e1c971d828
Jeriming <316906528@qq.com>
2024-03-07 20:48:29 +0800

    fixed: 初始化暗黑模式赋值修正

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { ref } from "vue";
 import { useDark, useToggle } from "@vueuse/core";
 
-const isDark = useDark();
+const DARK_STORAGE_KEY = 'useDark'
+const DARK_KEY = 'dark'
+const LIGHT_KEY = 'light'
+
+const isDark = useDark({
+  storageKey: DARK_STORAGE_KEY,
+  valueDark: DARK_KEY,
+  valueLight: LIGHT_KEY
+});
+
+const getFlag = () => {
+  return localStorage.getItem(DARK_STORAGE_KEY)
+}
 const toggleDark = useToggle(isDark);
 
 const App = (props) => {
-  const darkTheme = ref(false);
+  const darkTheme = ref(getFlag() === DARK_KEY ? true : false);
 
   const onChangeTheme = () => {
     toggleDark();
